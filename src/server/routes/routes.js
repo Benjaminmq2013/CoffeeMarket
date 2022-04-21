ea2fac1f6418b12d35a07d52ea04c172d3c6635e
benjaminmq2013 <61093891+Benjaminmq2013@users.noreply.github.com>
2022-04-21 22:43:01 +0200

    XML SOAP request is now working

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -9,16 +9,30 @@ const config = {
     }
 }
 
+let xmls ='<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:wspg">\
+  <soapenv:Header/>\
+  <soapenv:Body>\
+    <urn:connect soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\
+        <uid xsi:type="xsd:string">8c41704c487945ebd75d10cfed199d4b</uid>\
+        <wsk xsi:type="xsd:string">3ee3b69185e7936122aaa5355ed8cf85</wsk>\
+        <format_return xsi:type="xsd:string">json</format_return>\
+      </urn:connect>\
+   </soapenv:Body>\
+</soapenv:Envelope>'
+
 router.get("/payout", (req, res) =>{
   
-    axios.get('https://sandbox.pagadito.com/comercios/wspg/charges.php', config)
-  .then(function (response) {
-    
-    console.log(response);
-  })
-  .catch(function (error) {    
-    console.log(error);
-  })
+  axios
+    .post('https://sandbox.pagadito.com:443/comercios/wspg/charges.php', xmls,{
+      headers: { "Content-Type": "text/xml" },
+    })
+    .then(function (response) {
+      
+      console.log(response);
+    })
+    .catch(function (error) {    
+      console.log(error);
+    })
 
     // res.send("Pago aceptado!")
 })
